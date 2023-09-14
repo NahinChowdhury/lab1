20512791e675029403a44469d2c1734012867fce
Nahin Chowdhury <nahin@cs.ualberta.ca>
2023-09-13 18:27:56 -0600

    raw url pushed

--- script.py
+++ script.py
@@ -3,3 +3,5 @@ import requests
 print(requests.__version__)
 
 print(requests.get("https://www.google.com/teapot").text)
+
+print(requests.get("https://raw.githubusercontent.com/NahinChowdhury/lab1/master/script.py").text)
